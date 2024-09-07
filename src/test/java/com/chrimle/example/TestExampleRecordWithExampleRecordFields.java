f34389e184833f417ae27236a7edc00225eb9bd8
Chrimle <christopher.molin98@gmail.com>
2024-09-07 18:09:09 +0200

    Add tests for Record with Record fields

---
+++ src/test/java/com/chrimle/example/TestExampleRecordWithExampleRecordFields.java
@@ -0,0 +1,30 @@
+package com.chrimle.example;
+
+import com.chrimle.example.utils.AssertionUtils;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testing Record with ExampleRecord fields")
+public class TestExampleRecordWithExampleRecordFields {
+
+  private final Class<?> classUnderTest = ExampleRecordWithExampleRecordFields.class;
+
+  @Test
+  @DisplayName("Testing class generated is a Record")
+  public void testClassGeneratedIsRecord() {
+    AssertionUtils.assertIsRecord(classUnderTest);
+  }
+
+  @Test
+  @DisplayName("Testing generated Record is not annotated as @Deprecated")
+  public void testRecordIsNotAnnotatedAsDeprecated() {
+    AssertionUtils.assertRecordIsNotAnnotatedAsDeprecated(classUnderTest);
+  }
+
+  @Test
+  @DisplayName("Testing generated Record has expected number of fields of expected types")
+  public void testRecordHasNumberOfFieldsOfType() {
+    AssertionUtils.assertRecordHasNumberOfFieldsOfType(
+        classUnderTest, 3, ExampleRecord.class);
+  }
+}
