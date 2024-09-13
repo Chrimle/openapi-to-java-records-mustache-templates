58e230f640857f28507f62c082a9060722dbfd16
Christopher Molin <28791817+Chrimle@users.noreply.github.com>
2024-09-13 19:55:26 +0200

    Generate models with different `configuration`-options (#52)
    * Refactor `configuration`-values to properties
    
    * Move tests and generated sources to 'standard'-package
    
    * Add plugin-execution for `useEnumCaseInsensitive`

---
+++ src/test/java/com/chrimle/example/useEnumCaseInsensitive/TestExampleRecordWithNumberFields.java
@@ -0,0 +1,32 @@
+package com.chrimle.example.useEnumCaseInsensitive;
+
+import com.chrimle.example.utils.AssertionUtils;
+import java.math.BigDecimal;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Testing Record with Number fields")
+public class TestExampleRecordWithNumberFields {
+
+  private final Class<?> classUnderTest = ExampleRecordWithNumberFields.class;
+
+  @Test
+  @DisplayName("Testing class generated is a Record")
+  public void testClassGeneratedIsRecord() {
+    AssertionUtils.assertIsRecord(classUnderTest);
+  }
+
+  @Test
+  @DisplayName("Testing generated Record is not annotated as @Deprecated")
+  public void testRecordIsNotAnnotatedAsDeprecated() {
+    AssertionUtils.assertClassIsNotAnnotatedAsDeprecated(classUnderTest);
+  }
+
+  @Test
+  @DisplayName("Testing generated Record has expected number of fields of expected types")
+  public void testRecordHasNumberOfFieldsOfType() {
+    AssertionUtils.assertRecordHasNumberOfFieldsOfType(
+        classUnderTest, 3, BigDecimal.class);
+  }
+
+}
