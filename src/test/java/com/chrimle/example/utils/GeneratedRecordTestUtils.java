370c50b9448aabdf80d45e1ecd00201705a42755
Christopher Molin <28791817+Chrimle@users.noreply.github.com>
2024-09-18 22:06:09 +0200

    Refactor Test-classes (#69)
    * Refactor 'exampleRecord'-tests
    
    * Refactor 'DepreatedExampleRecord'-tests
    
    * Refactor 'exampleRecordWithArrayFields'-tests
    
    * Refactor 'exampleRecordWithBooleanFields'-tests
    
    * Refactor 'exampleRecordWithNumberFields'-tests
    
    * Refactor 'exampleRecordWithStringFields'-tests
    
    * Refactor 'exampleRecordWithIntegerFields'-tests
    
    * Refactor 'exampleEnum'-tests
    
    * Refactor 'deprecatedExampleEnum'-tests
    
    * Refactor 'exmapleRecordWithExampleEnumFields'-tests
    
    * Refactor 'exampleRecordWithExampleRecordFields'-tests
    
    * Refactor all model-tests to `TestSuite.java`
    
    * Refactor 'exampleRecordWithDefaultFields'-tests
    
    * Add Constructor & field assertion-methods
    
    * Refactor 'Record has fields'-tests
    
    * Refactor 'Record field'-tests
    
    * Refactor 'Class has Method'-tests
    
    * Refactor 'Record has/does not have Method'-tests

--- src/test/java/com/chrimle/example/utils/GeneratedRecordTestUtils.java
+++ src/test/java/com/chrimle/example/utils/GeneratedRecordTestUtils.java
@@ -1,5 +1,9 @@
 package com.chrimle.example.utils;
 
+import java.lang.reflect.Constructor;
+import java.math.BigDecimal;
+import java.util.List;
+
 /**
  * Generalized Test-class for testing Generated Record-classes
  */
@@ -20,6 +24,199 @@ public class GeneratedRecordTestUtils {
         isSerializableModel);
     AssertionUtils.assertRecordHasFieldsOfType(classUnderTest,
         isSerializableModel, fieldClasses);
+  }
+
+  public static void assertExampleRecord(
+      final Class<?> classUnderTest,
+      final boolean hasAdditionalTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        false,
+        hasAdditionalTypeAnnotations,
+        isSerializableModel,
+        Boolean.class
+    );
+  }
+
+  public static void assertDeprecatedExampleRecord(
+      final Class<?> classUnderTest,
+      final boolean hasAdditionalTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        true,
+        hasAdditionalTypeAnnotations,
+        isSerializableModel,
+        Boolean.class
+    );
+  }
+
+  public static void assertExampleRecordWithArrayFields(
+      final Class<?> classUnderTest,
+      final boolean hasAdditionalTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        false,
+        hasAdditionalTypeAnnotations,
+        isSerializableModel,
+        List.class,
+        List.class,
+        List.class
+    );
+  }
+
+  public static void assertExampleRecordWithBooleanFields(
+      final Class<?> classUnderTest,
+      final boolean hasAdditionalTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        false,
+        hasAdditionalTypeAnnotations,
+        isSerializableModel,
+        Boolean.class,
+        Boolean.class,
+        Boolean.class
+    );
+  }
+
+  public static void assertExampleRecordWithNumberFields(
+      final Class<?> classUnderTest,
+      final boolean hasAdditionalTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        false,
+        hasAdditionalTypeAnnotations,
+        isSerializableModel,
+        BigDecimal.class,
+        BigDecimal.class,
+        BigDecimal.class
+    );
+  }
+
+  public static void assertExampleRecordWithStringFields(
+      final Class<?> classUnderTest,
+      final boolean hasAdditionalTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        false,
+        hasAdditionalTypeAnnotations,
+        isSerializableModel,
+        String.class,
+        String.class,
+        String.class
+    );
+  }
+
+  public static void assertExampleRecordWithIntegerFields(
+      final Class<?> classUnderTest,
+      final boolean hasAdditionalTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        false,
+        hasAdditionalTypeAnnotations,
+        isSerializableModel,
+        Integer.class,
+        Integer.class,
+        Integer.class
+    );
+  }
+
+  public static <E extends Enum<E>> void assertExampleRecordWithExampleEnumFields(
+      final Class<?> classUnderTest,
+      final Class<E> enumClass,
+      final boolean hasAdditionalTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        false,
+        hasAdditionalTypeAnnotations,
+        isSerializableModel,
+        enumClass,
+        enumClass,
+        enumClass
+    );
+  }
+
+  public static void assertExampleRecordWithExampleRecordFields(
+      final Class<?> classUnderTest,
+      final Class<?> subClass,
+      final boolean hasAdditionalModelTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        false,
+        hasAdditionalModelTypeAnnotations,
+        isSerializableModel,
+        subClass,
+        subClass,
+        subClass
+    );
+  }
+
+  public static void assertExampleRecordWithDefaultFields(
+      final Class<?> classUnderTest,
+      final boolean hasAdditionalModelTypeAnnotations,
+      final boolean isSerializableModel
+  ) {
+    assertRecord(
+        classUnderTest,
+        false,
+        hasAdditionalModelTypeAnnotations,
+        isSerializableModel,
+        String.class,
+        String.class
+    );
+
+    final Constructor<?> constructor = AssertionUtils.assertRecordHasConstructor(
+        classUnderTest, String.class, String.class);
+
+    AssertionUtils.assertRecordFieldHasValue(
+        AssertionUtils.assertRecordInstantiateWithArgs(
+            classUnderTest,
+            constructor,
+            null,
+            null
+        ),
+        "field1",
+        null
+    );
+
+    AssertionUtils.assertRecordFieldHasValue(
+        AssertionUtils.assertRecordInstantiateWithArgs(
+            classUnderTest,
+            constructor,
+            "ignore",
+            null
+        ),
+        "field2",
+        "someDefaultValue"
+    );
+
+    AssertionUtils.assertRecordFieldHasValue(
+        AssertionUtils.assertRecordInstantiateWithArgs(
+            classUnderTest,
+            constructor,
+            "ignore",
+            "someValue"
+        ),
+        "field2",
+        "someValue"
+    );
   }
 
 }
