c120cc7db3156c872235ae8201d717a0bd77e086
Chrimle <christopher.molin98@gmail.com>
2024-09-25 22:01:46 +0200

    Refactor configuration-setup for each PluginExecution

--- src/test/java/com/chrimle/example/PluginExecution.java
+++ src/test/java/com/chrimle/example/PluginExecution.java
@@ -1,20 +1,98 @@
 package com.chrimle.example;
 
 public enum PluginExecution {
-  ADDITIONAL_ENUM_TYPE_ANNOTATIONS("additionalEnumTypeAnnotations"),
-  ADDITIONAL_MODEL_TYPE_ANNOTATIONS("additionalModelTypeAnnotations"),
-  GENERATE_BUILDERS("generateBuilders"),
-  SERIALIZABLE_MODEL("serializableModel"),
-  STANDARD("standard"),
-  USE_ENUM_CASE_INSENSITIVE("useEnumCaseInsensitive");
+  GENERATE_BUILDERS(
+      "generateBuilders",
+      true,
+      false,
+      false,
+      false,
+      false
+  ),
+  ADDITIONAL_ENUM_TYPE_ANNOTATIONS(
+      "additionalEnumTypeAnnotations",
+      false,
+      true,
+      false,
+      false,
+      false
+  ),
+  ADDITIONAL_MODEL_TYPE_ANNOTATIONS(
+      "additionalModelTypeAnnotations",
+      false,
+      false,
+      true,
+      false,
+      false
+  ),
+  SERIALIZABLE_MODEL(
+      "serializableModel",
+      false,
+      false,
+      false,
+      true,
+      false
+  ),
+  STANDARD(
+      "standard",
+      false,
+      false,
+      false,
+      false,
+      false
+  ),
+  USE_ENUM_CASE_INSENSITIVE(
+      "useEnumCaseInsensitive",
+      false,
+      false,
+      false,
+      false,
+      true
+  );
 
   private final String packageName;
+  private final boolean generateBuilders;
+  private final boolean hasAdditionalEnumTypeAnnotations;
+  private final boolean hasAdditionalModelTypeAnnotations;
+  private final boolean serializableModel;
+  private final boolean useEnumCaseInsensitive;
 
-  PluginExecution(final String packageName) {
+  PluginExecution(
+      final String packageName,
+      final boolean generateBuilders,
+      final boolean hasAdditionalEnumTypeAnnotations,
+      final boolean hasAdditionalModelTypeAnnotations,
+      final boolean serializableModel,
+      final boolean useEnumCaseInsensitive) {
     this.packageName = packageName;
+    this.hasAdditionalEnumTypeAnnotations = hasAdditionalEnumTypeAnnotations;
+    this.hasAdditionalModelTypeAnnotations = hasAdditionalModelTypeAnnotations;
+    this.generateBuilders = generateBuilders;
+    this.serializableModel = serializableModel;
+    this.useEnumCaseInsensitive = useEnumCaseInsensitive;
   }
 
   public String getPackageName() {
     return packageName;
+  }
+
+  public boolean generateBuilders() {
+    return generateBuilders;
+  }
+
+  public boolean hasAdditionalEnumTypeAnnotations() {
+    return hasAdditionalEnumTypeAnnotations;
+  }
+
+  public boolean hasAdditionalModelTypeAnnotations() {
+    return hasAdditionalModelTypeAnnotations;
+  }
+
+  public boolean serializableModel() {
+    return serializableModel;
+  }
+
+  public boolean useEnumCaseInsensitive() {
+    return useEnumCaseInsensitive;
   }
 }
--- src/test/java/com/chrimle/example/TestSuite.java
+++ src/test/java/com/chrimle/example/TestSuite.java
@@ -7,87 +7,27 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class TestSuite {
 
   @ParameterizedTest(name = "Testing: {0}")
-  @MethodSource("provideTestArguments")
-  @DisplayName("Testing Plugin Execution...")
-  public void testAll(
-      PluginExecution pluginExecution,
-      boolean hasAdditionalModelTypeAnnotations,
-      boolean hasAdditionalEnumTypeAnnotations,
-      boolean useEnumCaseInsensitive,
-      boolean isModelSerializable,
-      boolean generateBuilders) {
-
-    for (GeneratedClass generatedClass : GeneratedClass.values()) {
+  @EnumSource(PluginExecution.class)
+  @DisplayName("Testing Plugin Executions...")
+  public void testAll(final PluginExecution pluginExecution) {
+    for (final GeneratedClass generatedClass : GeneratedClass.values()) {
       assertModel(
           generatedClass,
           pluginExecution,
-          hasAdditionalModelTypeAnnotations,
-          hasAdditionalEnumTypeAnnotations,
-          useEnumCaseInsensitive,
-          isModelSerializable,
-          generateBuilders
+          pluginExecution.hasAdditionalModelTypeAnnotations(),
+          pluginExecution.hasAdditionalEnumTypeAnnotations(),
+          pluginExecution.useEnumCaseInsensitive(),
+          pluginExecution.serializableModel(),
+          pluginExecution.generateBuilders()
       );
     }
   }
-
-  public static Stream<Arguments> provideTestArguments() {
-    return Stream.of(
-        Arguments.of(
-            PluginExecution.ADDITIONAL_ENUM_TYPE_ANNOTATIONS,
-            false,
-            true,
-            false,
-            false,
-            false
-        ),
-        Arguments.of(
-            PluginExecution.ADDITIONAL_MODEL_TYPE_ANNOTATIONS,
-            true,
-            false,
-            false,
-            false,
-            false
-        ),
-        Arguments.of(
-            PluginExecution.GENERATE_BUILDERS,
-            false,
-            false,
-            false,
-            false,
-            true
-        ),
-        Arguments.of(
-            PluginExecution.SERIALIZABLE_MODEL,
-            false,
-            false,
-            false,
-            true,
-            false
-        ),
-        Arguments.of(
-            PluginExecution.STANDARD,
-            false,
-            false,
-            false,
-            false,
-            false
-        ),
-        Arguments.of(
-            PluginExecution.USE_ENUM_CASE_INSENSITIVE,
-            false,
-            false,
-            true,
-            false,
-            false
-        )
-    );
-  }
-
 
   private void assertModel(
       GeneratedClass generatedClass,
