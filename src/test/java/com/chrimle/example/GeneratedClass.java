15a55dfd846c16e0ac7c69b4acc33b71504f30ba
Chrimle <christopher.molin98@gmail.com>
2024-09-26 18:06:53 +0200

    Remove redundant records

--- src/test/java/com/chrimle/example/GeneratedClass.java
+++ src/test/java/com/chrimle/example/GeneratedClass.java
@@ -5,27 +5,14 @@ public enum GeneratedClass {
   DEPRECATED_EXAMPLE_RECORD("DeprecatedExampleRecord"),
   EXAMPLE_ENUM("ExampleEnum"),
   EXAMPLE_RECORD("ExampleRecord"),
-  EXAMPLE_RECORD_WITH_ARRAY_FIELDS("ExampleRecordWithArrayFields"),
-  EXAMPLE_RECORD_WITH_BOOLEAN_FIELDS("ExampleRecordWithBooleanFields"),
   EXAMPLE_RECORD_WITH_DEFAULT_FIELDS("ExampleRecordWithDefaultFields"),
-  EXAMPLE_RECORD_WITH_EXAMPLE_ENUM_FIELDS("ExampleRecordWithExampleEnumFields"),
-  EXAMPLE_RECORD_WITH_EXAMPLE_RECORD_FIELDS(
-      "ExampleRecordWithExampleRecordFields"),
-  EXAMPLE_RECORD_WITH_INTEGER_FIELDS("ExampleRecordWithIntegerFields"),
-  EXAMPLE_RECORD_WITH_NUMBER_FIELDS("ExampleRecordWithNumberFields"),
-  EXAMPLE_RECORD_WITH_SET_FIELDS("ExampleRecordWithSetFields"),
-  EXAMPLE_RECORD_WITH_STRING_FIELDS("ExampleRecordWithStringFields"),
   EXAMPLE_RECORD_WITH_REQUIRED_FIELDS_OF_EACH_TYPE("ExampleRecordWithRequiredFieldsOfEachType");
 
 
   private final String simpleClassName;
 
-  private GeneratedClass(final String simpleClassName) {
+  GeneratedClass(final String simpleClassName) {
     this.simpleClassName = simpleClassName;
-  }
-
-  public String getSimpleClassName() {
-    return simpleClassName;
   }
 
   public String getCanonicalClassName(final String moduleName) {
