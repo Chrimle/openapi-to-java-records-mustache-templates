b2fa457bde26a4ecab64ef991951a4ec1389f43a
Chrimle <christopher.molin98@gmail.com>
2024-10-31 00:00:54 +0100

    Make temporary change to see Test Coverage Report

--- target/generated-sources/openapi/src/src/gen/java/main/io/github/chrimle/example/standard/DeprecatedExampleEnum.java
+++ target/generated-sources/openapi/src/src/gen/java/main/io/github/chrimle/example/standard/DeprecatedExampleEnum.java
@@ -23,10 +23,7 @@ import com.google.gson.annotations.SerializedName;
 
 /**
  * Example of a deprecated Enum
- *
- * @deprecated
  */
-@Deprecated
 public enum DeprecatedExampleEnum {
   ENUM1("ENUM1"),
   ENUM2("ENUM2"),
--- target/generated-sources/openapi/src/src/gen/java/main/io/github/chrimle/example/useBeanValidation/DeprecatedExampleEnum.java
+++ target/generated-sources/openapi/src/src/gen/java/main/io/github/chrimle/example/useBeanValidation/DeprecatedExampleEnum.java
@@ -25,10 +25,7 @@ import jakarta.validation.Valid;
 
 /**
  * Example of a deprecated Enum
- *
- * @deprecated
  */
-@Deprecated
 public enum DeprecatedExampleEnum {
   ENUM1("ENUM1"),
   ENUM2("ENUM2"),
