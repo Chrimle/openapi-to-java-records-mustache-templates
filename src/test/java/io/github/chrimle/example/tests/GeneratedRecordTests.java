3f5b87eacc90a1832fdecd6d839a1b33ad541a89
Chrimle <christopher.molin98@gmail.com>
2024-12-07 22:46:32 +0100

    Refactor `GeneratedRecordTests`

--- src/test/java/io/github/chrimle/example/tests/GeneratedRecordTests.java
+++ src/test/java/io/github/chrimle/example/tests/GeneratedRecordTests.java
@@ -1,6 +1,13 @@
 package io.github.chrimle.example.tests;
 
 import io.github.chrimle.example.GeneratedSource;
+import io.github.chrimle.example.tests.GeneratedRecordTests.GeneratorConfigurationTests.ConfigOptionsTests;
+import io.github.chrimle.example.tests.GeneratedRecordTests.OpenAPITests.SchemaTests;
+import io.github.chrimle.example.tests.GeneratedRecordTests.OpenAPITests.SchemaTests.DeprecatedTests;
+import io.github.chrimle.example.tests.GeneratedRecordTests.OpenAPITests.SchemaTests.PropertiesTests;
+import io.github.chrimle.example.tests.GeneratedRecordTests.OpenAPITests.SchemaTests.PropertiesTests.PropertyTests;
+import io.github.chrimle.example.tests.GeneratedRecordTests.OpenAPITests.SchemaTests.TypeTests;
+import io.github.chrimle.example.tests.GeneratedRecordTests.OpenAPITests.SchemaTests.XClassExtraAnnotationTests;
 import io.github.chrimle.example.utils.AssertionUtils;
 import io.github.chrimle.example.utils.GeneratedRecordTestUtils;
 import org.junit.jupiter.api.DisplayName;
@@ -8,139 +15,206 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-/** Class for testing generated {@code record} classes. */
+/**
+ * Tests for <b>all</b> generated {@code record} classes. Tests are grouped by the <i>method of
+ * configuring</i> the resulting {@code record}.
+ *
+ * <p>
+ *
+ * <h2>Table of Contents</h2>
+ *
+ * <i>All tests are categorized and grouped accordingly, using {@link Nested} classes. This is the
+ * complete list of tested <i>properties</i> and <i>configurations</i>.<br>
+ *
+ * <p><b>OpenAPI Specification</b>
+ *
+ * <p>{@link OpenAPITests}
+ *
+ * <ul>
+ *   <li>{@code components.schemas.{schema}}
+ *       <p>{@link SchemaTests}
+ *   <li>{@code components.schemas.{schema}.type}
+ *       <p>{@link TypeTests}
+ *   <li>{@code components.schemas.{schema}.deprecated}
+ *       <p>{@link DeprecatedTests}
+ *   <li>{@code components.schemas.{schema}.x-class-extra-annotation}
+ *       <p>{@link XClassExtraAnnotationTests}
+ *   <li>{@code components.schemas.{schema}.properties}
+ *       <p>{@link PropertiesTests}
+ *   <li>{@code components.schemas.{schema}.properties.{property}}
+ *       <p>{@link PropertyTests}
+ * </ul>
+ *
+ * <p><b>`openapi-generator` Configurations</b>
+ *
+ * <p>{@link GeneratorConfigurationTests}
+ *
+ * <ul>
+ *   <li>{@code <configOptions>}
+ *       <p>{@link ConfigOptionsTests}
+ * </ul>
+ */
 final class GeneratedRecordTests implements GeneratedClassTests {
 
-  /**
-   * Test-class for asserting functionalities, properties and behaviours configured via OpenAPI
-   * Specifications.
-   */
   @Nested
   @DisplayName("Testing OpenAPI Schemas & Properties")
   class OpenAPITests {
 
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName("OpenAPI `{schema}.type: object` -> Generates a `record` class")
-    public void whenIsObjectThenGeneratedClassIsRecordClass(final GeneratedSource generatedSource) {
-      AssertionUtils.assertIsRecord(generatedSource.getClassUnderTest());
-    }
+    @Nested
+    @DisplayName("Testing `components.schemas.{schema}`")
+    class SchemaTests {
 
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "OpenAPI `{schema}.properties` -> Generates a constructor with properties as method arguments")
-    public void whenObjectHasPropertiesThenGeneratedConstructorHasMethodArguments(
-        final GeneratedSource generatedSource) {
-      AssertionUtils.assertRecordHasConstructor(
-          generatedSource.getClassUnderTest(), generatedSource.fieldClasses());
-    }
+      @Nested
+      @DisplayName("Testing `components.schemas.{schema}.type`")
+      class TypeTests {
 
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "OpenAPI `{schema}.properties.{property}` -> Instantiating the `record` will set fields to provided values")
-    public void whenObjectHasPropertiesThenFieldIsSetToProvidedValueWhenInstantiatingRecord(
-        final GeneratedSource generatedSource) {
-      GeneratedRecordTestUtils.assertInstantiatingRecordWithValuesSetsFieldsToProvidedValue(
-          generatedSource);
-    }
-
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "OpenAPI `{schema}.properties.{property}.default` -> Instantiating the `record` with `null` will set fields to `null` or `default`")
-    public void whenPropertyHasDefaultSetThenFieldIsSetToDefaultValueWhenInstantiatingWithNull(
-        final GeneratedSource generatedSource) {
-      GeneratedRecordTestUtils.assertInstantiatingRecordWithNullSetsFieldsToNullOrDefaultValue(
-          generatedSource);
-    }
-
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "OpenAPI `{schema}.properties.{property}.nullable` -> Fields of generated `record` are annotated with `@Nullable` and `@Nonnull`")
-    public void whenPropertyHasNullableSetThenFieldIsAnnotatedWithNullableOrNonnull(
-        final GeneratedSource generatedSource) {
-      AssertionUtils.assertRecordHasFieldsOfTypeWithNullableAnnotations(generatedSource);
-    }
-
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "OpenAPI `{schema}.deprecated` -> Annotates generated `record` class with `@Deprecated`")
-    public void whenRecordIsDeprecatedThenGeneratedRecordClassIsAnnotatedDeprecated(
-        final GeneratedSource generatedSource) {
-      AssertionUtils.assertClassIsAnnotatedAsDeprecated(
-          generatedSource.getClassUnderTest(), generatedSource.isDeprecated());
-    }
-
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "OpenAPI `{schema}.x-class-extra-annotation` -> Annotates generated `record` class with extra annotations")
-    public void
-        whenRecordHasExtraClassAnnotationsThenGeneratedRecordClassIsAnnotatedWithExtraAnnotations(
+        @ParameterizedTest
+        @MethodSource(
+            "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+        @DisplayName("OpenAPI `{schema}.type: object` -> Generates a `record` class")
+        public void whenIsObjectThenGeneratedClassIsRecordClass(
             final GeneratedSource generatedSource) {
-      GeneratedRecordTestUtils.assertClassIsAnnotatedWithExtraAnnotation(generatedSource);
+          AssertionUtils.assertIsRecord(generatedSource.getClassUnderTest());
+        }
+      }
+
+      @Nested
+      @DisplayName("Testing `components.schemas.{schema}.deprecated`")
+      class DeprecatedTests {
+        @ParameterizedTest
+        @MethodSource(
+            "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+        @DisplayName(
+            "OpenAPI `{schema}.deprecated` -> Annotates generated `record` class with `@Deprecated`")
+        public void whenRecordIsDeprecatedThenGeneratedRecordClassIsAnnotatedDeprecated(
+            final GeneratedSource generatedSource) {
+          AssertionUtils.assertClassIsAnnotatedAsDeprecated(
+              generatedSource.getClassUnderTest(), generatedSource.isDeprecated());
+        }
+      }
+
+      @Nested
+      @DisplayName("Testing `components.schemas.{schema}.x-class-extra-annotation`")
+      class XClassExtraAnnotationTests {
+
+        @ParameterizedTest
+        @MethodSource(
+            "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+        @DisplayName(
+            "OpenAPI `{schema}.x-class-extra-annotation` -> Annotates generated `record` class with extra annotations")
+        public void
+            whenRecordHasExtraClassAnnotationsThenGeneratedRecordClassIsAnnotatedWithExtraAnnotations(
+                final GeneratedSource generatedSource) {
+          GeneratedRecordTestUtils.assertClassIsAnnotatedWithExtraAnnotation(generatedSource);
+        }
+      }
+
+      @Nested
+      @DisplayName("Testing `components.schemas.{schema}.properties`")
+      class PropertiesTests {
+
+        @ParameterizedTest
+        @MethodSource(
+            "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+        @DisplayName(
+            "OpenAPI `{schema}.properties` -> Generates a constructor with properties as method arguments")
+        public void whenObjectHasPropertiesThenGeneratedConstructorHasMethodArguments(
+            final GeneratedSource generatedSource) {
+          AssertionUtils.assertRecordHasConstructor(
+              generatedSource.getClassUnderTest(), generatedSource.fieldClasses());
+        }
+
+        @ParameterizedTest
+        @MethodSource(
+            "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+        @DisplayName(
+            "OpenAPI `{schema}.properties.{property}` -> Instantiating the `record` will set fields to provided values")
+        public void whenObjectHasPropertiesThenFieldIsSetToProvidedValueWhenInstantiatingRecord(
+            final GeneratedSource generatedSource) {
+          GeneratedRecordTestUtils.assertInstantiatingRecordWithValuesSetsFieldsToProvidedValue(
+              generatedSource);
+        }
+
+        @Nested
+        @DisplayName("Testing `components.schemas.{schema}.properties.{property}`")
+        class PropertyTests {
+
+          @ParameterizedTest
+          @MethodSource(
+              "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+          @DisplayName(
+              "OpenAPI `{schema}.properties.{property}.default` -> Instantiating the `record` with `null` will set fields to `null` or `default`")
+          public void
+              whenPropertyHasDefaultSetThenFieldIsSetToDefaultValueWhenInstantiatingWithNull(
+                  final GeneratedSource generatedSource) {
+            GeneratedRecordTestUtils
+                .assertInstantiatingRecordWithNullSetsFieldsToNullOrDefaultValue(generatedSource);
+          }
+
+          @ParameterizedTest
+          @MethodSource(
+              "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+          @DisplayName(
+              "OpenAPI `{schema}.properties.{property}.nullable` -> Fields of generated `record` are annotated with `@Nullable` and `@Nonnull`")
+          public void whenPropertyHasNullableSetThenFieldIsAnnotatedWithNullableOrNonnull(
+              final GeneratedSource generatedSource) {
+            AssertionUtils.assertRecordHasFieldsOfTypeWithNullableAnnotations(generatedSource);
+          }
+        }
+      }
     }
   }
 
-  /**
-   * Test-class for asserting functionalities, properties and behaviours configured via
-   * `openapi-generator`'s {@code <configuration>} and {@code <configOptions>}.
-   */
   @Nested
   @DisplayName("Testing `openapi-generator` Configurations & ConfigOptions")
   class GeneratorConfigurationTests {
 
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "Configuration `configOptions.additionalModelTypeAnnotations` -> Annotates generated `record` class with additional annotations")
-    void
-        whenConfigOptionAdditionalModelTypeAnnotationsIsSetThenGeneratedRecordClassHasAdditionalAnnotations(
-            final GeneratedSource generatedSource) {
-      AssertionUtils.assertClassIsAnnotatedWithAdditionalTypeAnnotations(
-          generatedSource.getClassUnderTest(), generatedSource.hasAdditionalModelTypeAnnotations());
-    }
+    @Nested
+    @DisplayName("Testing `<configOptions>`")
+    class ConfigOptionsTests {
 
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "Configuration `configOptions.serializableModel` -> Generated `record` has `serialVersionUID`-field")
-    void whenConfigOptionSerializableModelIsSetThenGeneratedRecordClassHasSerialVersionUidField(
-        final GeneratedSource generatedSource) {
-      AssertionUtils.assertModelIsSerializable(generatedSource);
-    }
+      @ParameterizedTest
+      @MethodSource(
+          "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+      @DisplayName(
+          "Configuration `configOptions.additionalModelTypeAnnotations` -> Annotates generated `record` class with additional annotations")
+      void
+          whenConfigOptionAdditionalModelTypeAnnotationsIsSetThenGeneratedRecordClassHasAdditionalAnnotations(
+              final GeneratedSource generatedSource) {
+        AssertionUtils.assertClassIsAnnotatedWithAdditionalTypeAnnotations(
+            generatedSource.getClassUnderTest(),
+            generatedSource.hasAdditionalModelTypeAnnotations());
+      }
 
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "Configuration `configOptions.serializableModel` -> Generated `record` implements `Serializable`")
-    void whenConfigOptionSerializableModelIsSetThenGeneratedRecordClassImplementsSerializable(
-        final GeneratedSource generatedSource) {
-      AssertionUtils.assertClassImplementsSerializable(generatedSource);
-    }
+      @ParameterizedTest
+      @MethodSource(
+          "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+      @DisplayName(
+          "Configuration `configOptions.serializableModel` -> Generated `record` has `serialVersionUID`-field")
+      void whenConfigOptionSerializableModelIsSetThenGeneratedRecordClassHasSerialVersionUidField(
+          final GeneratedSource generatedSource) {
+        AssertionUtils.assertModelIsSerializable(generatedSource);
+      }
 
-    @ParameterizedTest
-    @MethodSource(
-        "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
-    @DisplayName(
-        "Configuration `configOptions.generateBuilders` -> Generated `record` has inner `Builder`-class")
-    void whenConfigOptionGenerateBuildersIsSetThenGeneratedRecordHasInnerBuilderClass(
-        final GeneratedSource generatedSource) {
-      AssertionUtils.assertRecordHasBuilderInnerClass(generatedSource);
+      @ParameterizedTest
+      @MethodSource(
+          "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+      @DisplayName(
+          "Configuration `configOptions.serializableModel` -> Generated `record` implements `Serializable`")
+      void whenConfigOptionSerializableModelIsSetThenGeneratedRecordClassImplementsSerializable(
+          final GeneratedSource generatedSource) {
+        AssertionUtils.assertClassImplementsSerializable(generatedSource);
+      }
+
+      @ParameterizedTest
+      @MethodSource(
+          "io.github.chrimle.example.tests.GeneratedClassTests#allPluginExecutionsAndGeneratedRecordCombinations")
+      @DisplayName(
+          "Configuration `configOptions.generateBuilders` -> Generated `record` has inner `Builder`-class")
+      void whenConfigOptionGenerateBuildersIsSetThenGeneratedRecordHasInnerBuilderClass(
+          final GeneratedSource generatedSource) {
+        AssertionUtils.assertRecordHasBuilderInnerClass(generatedSource);
+      }
     }
   }
 }
