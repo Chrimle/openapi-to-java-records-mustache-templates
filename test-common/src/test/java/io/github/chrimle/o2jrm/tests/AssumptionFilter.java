7d712e7ce4599796f5f4e3f403994dd8a8643f42
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-04-05 22:58:20 +0200

    Add JavaDocs to `AssumptionFilter`-class

--- test-common/src/test/java/io/github/chrimle/o2jrm/tests/AssumptionFilter.java
+++ test-common/src/test/java/io/github/chrimle/o2jrm/tests/AssumptionFilter.java
@@ -23,32 +23,53 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Allows filtering {@link io.github.chrimle.o2jrm.GeneratedSource}s using {@link
+ * org.junit.jupiter.api.Assumptions}, based on several criteria.
+ */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AssumptionFilter {
 
+  /** The <em>Type</em> of the enum {@code value}. Must match exactly. Default allows all types. */
   Class<?> enumValueClass() default Object.class;
 
-
+  /** Whether {@link ConfigOption#ADDITIONAL_ENUM_TYPE_ANNOTATIONS} should be set. */
   Condition hasAdditionalEnumTypeAnnotations() default Condition.ANY;
 
+  /** Whether {@link ConfigOption#ADDITIONAL_MODEL_TYPE_ANNOTATIONS} should be set. */
   Condition hasAdditionalModelTypeAnnotations() default Condition.ANY;
 
+  /** Whether the schema has "extra annotations". */
   Condition hasExtraAnnotations() default Condition.ANY;
 
+  /** Whether the schema has required fields. */
   Condition hasRequiredGeneratedFields() default Condition.ANY;
 
+  /** Whether the schema has "x-implements". */
   Condition hasXImplements() default Condition.ANY;
 
+  /** Whether the schema is "deprecated". */
   Condition isDeprecated() default Condition.ANY;
 
+  /** Whether the schema is an "inner" enum class. */
   Condition isInnerEnum() default Condition.ANY;
 
+  /**
+   * Collection of {@link ConfigOption}s that ALL MUST be {@code true}. Other {@link ConfigOption}s
+   * (not referenced) MAY be false or true. Default allows any or all ConfigOptions to be false.
+   */
   ConfigOption[] enabledConfigOptions() default {};
 
+  /**
+   * Collection of {@link ConfigOption}s that ALL MUST be {@code false}. Other {@link ConfigOption}s
+   * (not referenced) MAY be false or true. Default allows any or all ConfigOptions to be true.
+   */
   ConfigOption[] disabledConfigOptions() default {};
 
+  /** Collection of {@link Library}s. Default allows all {@link Library}s. */
   Library[] isOneOfLibraries() default {};
 
+  /** Whether {@code jackson} serialization library is used. */
   Condition isSerializationLibraryJackson() default Condition.ANY;
 }
