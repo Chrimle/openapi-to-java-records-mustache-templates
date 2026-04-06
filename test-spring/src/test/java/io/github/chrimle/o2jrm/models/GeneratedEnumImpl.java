dbf5b4de033ac4f85204c275e54a7d3f21bdf04c
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-04-06 14:02:23 +0200

    Enable Enum Unit-Tests

---
+++ test-spring/src/test/java/io/github/chrimle/o2jrm/models/GeneratedEnumImpl.java
@@ -0,0 +1,172 @@
+/*
+ *   Copyright 2024-2026 Chrimle
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package io.github.chrimle.o2jrm.models;
+
+import io.github.chrimle.o2jrm.interfaces.TestInterfaceOne;
+import io.github.chrimle.o2jrm.interfaces.TestInterfaceTwo;
+import java.util.List;
+
+/** Enum class listing all expected {@code enum} classes to be generated from the OpenAPI spec. */
+@SuppressWarnings("ImmutableEnumChecker")
+public enum GeneratedEnumImpl implements GeneratedEnum, GeneratedClassImpl {
+  DEPRECATED_EXAMPLE_ENUM(
+      "DeprecatedExampleEnum",
+      true,
+      List.of(),
+      GeneratedField.of("ENUM1", String.class, "ENUM1").build(),
+      GeneratedField.of("ENUM2", String.class, "ENUM2").build(),
+      GeneratedField.of("ENUM3", String.class, "ENUM3").build()),
+  EXAMPLE_ENUM(
+      "ExampleEnum",
+      false,
+      List.of(),
+      GeneratedField.of("ENUM1", String.class, "ENUM1").build(),
+      GeneratedField.of("ENUM2", String.class, "ENUM2").build(),
+      GeneratedField.of("ENUM3", String.class, "ENUM3").build()),
+  EXAMPLE_NULLABLE_ENUM(
+      "ExampleNullableEnum",
+      false,
+      List.of(),
+      GeneratedField.of("ENUM1", String.class, "ENUM1").build(),
+      GeneratedField.of("ENUM2", String.class, "ENUM2").build(),
+      GeneratedField.of("ENUM3", String.class, "ENUM3").build()),
+  EXAMPLE_URI_ENUM(
+      "ExampleUriEnum",
+      false,
+      List.of(),
+      GeneratedField.of(
+              "HTTPS_GITHUB_COM_CHRIMLE_OPENAPI_TO_JAVA_RECORDS_MUSTACHE_TEMPLATES",
+              String.class, // URI.class,
+              "https://github.com/Chrimle/openapi-to-java-records-mustache-templates" // URI.create("https://github.com/Chrimle/openapi-to-java-records-mustache-templates")
+              )
+          .build(),
+      GeneratedField.of(
+              "HTTPS_CHRIMLE_GITHUB_IO_OPENAPI_TO_JAVA_RECORDS_MUSTACHE_TEMPLATES_",
+              String.class, // URI.class,
+              "https://chrimle.github.io/openapi-to-java-records-mustache-templates/" // URI.create("https://chrimle.github.io/openapi-to-java-records-mustache-templates/")
+              )
+          .build()),
+  EXAMPLE_ENUM_WITH_INTEGER_VALUES(
+      "ExampleEnumWithIntegerValues",
+      false,
+      List.of(),
+      GeneratedField.of("NUMBER_100", Integer.class, 100).build(),
+      GeneratedField.of("NUMBER_200", Integer.class, 200).build(),
+      GeneratedField.of("NUMBER_300", Integer.class, 300).build(),
+      GeneratedField.of("NUMBER_400", Integer.class, 400).build(),
+      GeneratedField.of("NUMBER_500", Integer.class, 500).build()),
+  //  EXAMPLE_INNER_ENUM(
+  //      "RecordWithInnerEnums$ExampleInnerEnum",
+  //      false,
+  //      List.of(),
+  //      GeneratedField.of("ENUM1", String.class, "ENUM1").build(),
+  //      GeneratedField.of("ENUM2", String.class, "ENUM2").build(),
+  //      GeneratedField.of("ENUM3", String.class, "ENUM3").build()),
+  //  EXAMPLE_INNER_TWO_ENUM(
+  //      "RecordWithInnerEnums$ExampleInnerTwoEnum",
+  //      false,
+  //      List.of(),
+  //      GeneratedField.of("NUMBER_404", Integer.class, 404).build(),
+  //      GeneratedField.of("NUMBER_501", Integer.class, 501).build(),
+  //      GeneratedField.of("NUMBER_503", Integer.class, 503).build()),
+  //  EXAMPLE_INNER_THREE_ENUM(
+  //      "RecordWithInnerEnums$ExampleInnerThreeEnum",
+  //      false,
+  //      List.of(),
+  //      GeneratedField.of(
+  //              "HTTPS_GITHUB_COM_CHRIMLE_OPENAPI_TO_JAVA_RECORDS_MUSTACHE_TEMPLATES",
+  //              URI.class,
+  //
+  // URI.create("https://github.com/Chrimle/openapi-to-java-records-mustache-templates"))
+  //          .build(),
+  //      GeneratedField.of(
+  //              "HTTPS_CHRIMLE_GITHUB_IO_OPENAPI_TO_JAVA_RECORDS_MUSTACHE_TEMPLATES_",
+  //              URI.class,
+  //
+  // URI.create("https://chrimle.github.io/openapi-to-java-records-mustache-templates/"))
+  //          .build()),
+  EXAMPLE_IMPLEMENTS_ENUM(
+      "ExampleImplementsEnum",
+      false,
+      List.of(TestInterfaceOne.class),
+      GeneratedField.of("ENUM1", String.class, "ENUM1").build(),
+      GeneratedField.of("ENUM2", String.class, "ENUM2").build(),
+      GeneratedField.of("ENUM3", String.class, "ENUM3").build()),
+  EXAMPLE_TWO_IMPLEMENTS_ENUM(
+      "ExampleTwoImplementsEnum",
+      false,
+      List.of(TestInterfaceOne.class, TestInterfaceTwo.class),
+      GeneratedField.of("ENUM1", String.class, "ENUM1").build(),
+      GeneratedField.of("ENUM2", String.class, "ENUM2").build(),
+      GeneratedField.of("ENUM3", String.class, "ENUM3").build());
+
+  private final String simpleClassName;
+  private final boolean isDeprecated;
+  private final List<Class<?>> implementsInterfaces;
+  private final GeneratedField<?>[] generatedFields;
+
+  GeneratedEnumImpl(
+      final String simpleClassName,
+      final boolean isDeprecated,
+      List<Class<?>> implementsInterfaces,
+      final GeneratedField<?>... generatedFields) {
+    this.simpleClassName = simpleClassName;
+    this.isDeprecated = isDeprecated;
+    this.implementsInterfaces = implementsInterfaces;
+    this.generatedFields = generatedFields;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @return the class name as a {@link String}.
+   */
+  @Override
+  public String getSimpleClassName() {
+    return simpleClassName;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @return whether the class is deprecated.
+   */
+  @Override
+  public boolean isDeprecated() {
+    return isDeprecated;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @return the collection of {@code interface}-classes to implement, or an empty collection if
+   *     none.
+   */
+  @Override
+  public List<Class<?>> getImplementsInterfaces() {
+    return implementsInterfaces;
+  }
+
+  /**
+   * {@inheritDoc}
+   *
+   * @return the collection of generatedFields.
+   */
+  @Override
+  public GeneratedField<?>[] getGeneratedFields() {
+    return generatedFields;
+  }
+}
---
+++ test-spring/src/test/java/io/github/chrimle/o2jrm/tests/GeneratedEnumImplTests.java
@@ -0,0 +1,67 @@
+/*
+ *   Copyright 2024-2026 Chrimle
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+package io.github.chrimle.o2jrm.tests;
+
+import io.github.chrimle.o2jrm.GeneratedSource;
+import io.github.chrimle.o2jrm.PluginExecutionImpl;
+import io.github.chrimle.o2jrm.models.BeanValidationAnnotation;
+import io.github.chrimle.o2jrm.models.GeneratedClass;
+import io.github.chrimle.o2jrm.models.GeneratedClassImpl;
+import io.github.chrimle.o2jrm.models.GeneratedEnumImpl;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.*;
+
+@DisplayName("Test Generated `enum` classes")
+final class GeneratedEnumImplTests extends GeneratedEnumTests {
+
+  @SuppressWarnings("unused")
+  static List<GeneratedSource> allPluginExecutionsAndGeneratedEnumCombinations() {
+    return Stream.of(PluginExecutionImpl.values())
+        .flatMap(
+            pluginExecution ->
+                Stream.of(GeneratedEnumImpl.values())
+                    .map(
+                        generatedEnum ->
+                            new GeneratedSource(
+                                pluginExecution,
+                                generatedEnum,
+                                GeneratedClassImpl.getGeneratedFields(
+                                    generatedEnum, pluginExecution),
+                                GeneratedClass.getClass(generatedEnum, pluginExecution),
+                                Map.of(
+                                    BeanValidationAnnotation.VALID,
+                                    javax.validation.Valid.class,
+                                    BeanValidationAnnotation.NOT_NULL,
+                                    javax.validation.constraints.NotNull.class,
+                                    BeanValidationAnnotation.PATTERN,
+                                    javax.validation.constraints.Pattern.class,
+                                    BeanValidationAnnotation.SIZE,
+                                    javax.validation.constraints.Size.class,
+                                    BeanValidationAnnotation.MIN,
+                                    javax.validation.constraints.Min.class,
+                                    BeanValidationAnnotation.MAX,
+                                    javax.validation.constraints.Max.class,
+                                    BeanValidationAnnotation.DECIMAL_MIN,
+                                    javax.validation.constraints.DecimalMin.class,
+                                    BeanValidationAnnotation.DECIMAL_MAX,
+                                    javax.validation.constraints.DecimalMax.class,
+                                    BeanValidationAnnotation.EMAIL,
+                                    javax.validation.constraints.Email.class))))
+        .toList();
+  }
+}
