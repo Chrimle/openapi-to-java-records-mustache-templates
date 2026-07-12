f468363b4835441c1f0ebd8d4ba23b5cbf13f14b
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-07-12 17:22:36 +0200

    Relocate Test Annotations
    Prerequisite for Java modularizing.

--- test-utils/src/main/java/io/github/chrimle/o2jrm/test/annotations/TestAnnotationTwo.java
+++ test-utils/src/main/java/io/github/chrimle/o2jrm/test/annotations/TestAnnotationTwo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.github.chrimle.o2jrm.annotations;
+package io.github.chrimle.o2jrm.test.annotations;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -21,5 +21,5 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.FIELD)
-public @interface TestFieldExtraAnnotationOne {}
+@Target(ElementType.TYPE)
+public @interface TestAnnotationTwo {}
