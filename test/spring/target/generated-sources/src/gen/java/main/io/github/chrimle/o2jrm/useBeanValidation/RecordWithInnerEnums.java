0063f47bd0a573dbd4e4774c4390a504062ef12a
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-07-20 13:17:08 +0200

    Update Generated Sources

--- test/spring/target/generated-sources/src/gen/java/main/io/github/chrimle/o2jrm/useBeanValidation/RecordWithInnerEnums.java
+++ test/spring/target/generated-sources/src/gen/java/main/io/github/chrimle/o2jrm/useBeanValidation/RecordWithInnerEnums.java
@@ -2,6 +2,7 @@ package io.github.chrimle.o2jrm.useBeanValidation;
 
 import java.net.URI;
 import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
