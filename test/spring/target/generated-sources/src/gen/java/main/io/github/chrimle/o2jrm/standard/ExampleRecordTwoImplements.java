5943b38349ec203f85c9d0123f84cc4167b676e7
Chrimle <28791817+Chrimle@users.noreply.github.com>
2026-09-03 21:56:41 +0200

    Update Generated Sources

--- test/spring/target/generated-sources/src/gen/java/main/io/github/chrimle/o2jrm/standard/ExampleRecordTwoImplements.java
+++ test/spring/target/generated-sources/src/gen/java/main/io/github/chrimle/o2jrm/standard/ExampleRecordTwoImplements.java
@@ -2,7 +2,6 @@ package io.github.chrimle.o2jrm.standard;
 
 import java.net.URI;
 import java.util.Objects;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import org.springframework.lang.Nullable;
